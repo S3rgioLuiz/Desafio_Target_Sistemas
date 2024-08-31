d97fb305ce266207143b8ed306ac53d12648689d
S3rgioLuiz <sergiorosario.pl012@academico.ifsul.edu.br>
2024-08-30 22:50:49 -0300

    Refatorando Exercício 02

--- Exercicio_02.js
+++ Exercicio_02.js
@@ -2,19 +2,19 @@ function pertenceFibonacci(numero) {
     let a = 0, b = 1;
     
     if (numero === a || numero === b) 
-        return `O número ${numero} pertence à sequência de Fibonacci.`;
+        return "O número "+ numero +" pertence à sequência de Fibonacci.";
     
     
     let proximo = a + b;
     while (proximo <= numero) {
         if (proximo === numero) {
-            return `O número ${numero} pertence à sequência de Fibonacci.`;
+            return "O número "+ numero +" pertence à sequência de Fibonacci.";
         }
         a = b;
         b = proximo;
         proximo = a + b;
     }
-    return `O número ${numero} não pertence à sequência de Fibonacci.`;
+    return "O número "+ numero +" não pertence à sequência de Fibonacci.";
 }
 
 
